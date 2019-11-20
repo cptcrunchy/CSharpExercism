729693bb21c2bb99ff1627ac6905b1404a6092db
Jason Gutierrez <jasong84@gmail.com>
2019-11-20 17:29:50 -0600

    Add my solution for the PythagoreanTriplet

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -1,10 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public static class PythagoreanTriplet
+namespace Exercism
 {
-    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    public static class PythagoreanTriplet
     {
-        throw new NotImplementedException("You need to implement this function.");
+        public static IEnumerable<(int sideA, int sideB, int sideC)> TripletsWithSum(int sum)
+        {
+            if(sum <= 5)
+                throw new ArgumentException("The sum must be greater than 5.");
+
+            return Enumerable
+                   .Range(1, sum - 1)
+                   .SelectMany(
+                        a => Enumerable.Range(a + 1, sum - a - 1)
+                        .Where(b => b < C(a, b, sum) && IsValid(a,b, C(a, b, sum)))
+                        .Select(b => new ValueTuple<int, int, int>(a, b, C(a, b, sum))));
+        }
+
+        private static int C(int a, int b, int sum) => sum - a - b;
+        private static bool IsValid(int a, int b, int c) => Math.Pow(a,2) + Math.Pow(b,2) == Math.Pow(c,2);
     }
 }
--- pythagorean-triplet/PythagoreanTripletTest.cs
+++ pythagorean-triplet/PythagoreanTripletTest.cs
@@ -3,56 +3,59 @@
 using System;
 using Xunit;
 
-public class PythagoreanTripletTest
+namespace Exercism
 {
-    [Fact]
-    public void Triplets_whose_sum_is_12()
+
+    public class PythagoreanTripletTest
     {
-        Assert.Equal(new[]
+        [Fact]
+        public void Triplets_whose_sum_is_12()
         {
+            Assert.Equal(new[]
+            {
             (3, 4, 5)
         }, PythagoreanTriplet.TripletsWithSum(12));
-    }
+        }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Triplets_whose_sum_is_108()
-    {
-        Assert.Equal(new[]
+        [Fact]
+        public void Triplets_whose_sum_is_108()
         {
+            Assert.Equal(new[]
+            {
             (27, 36, 45)
         }, PythagoreanTriplet.TripletsWithSum(108));
-    }
+        }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Triplets_whose_sum_is_1000()
-    {
-        Assert.Equal(new[]
+        [Fact]
+        public void Triplets_whose_sum_is_1000()
         {
+            Assert.Equal(new[]
+            {
             (200, 375, 425)
         }, PythagoreanTriplet.TripletsWithSum(1000));
-    }
+        }
 
-    [Fact(Skip = "Remove to run test")]
-    public void No_matching_triplets_for_1001()
-    {
-        Assert.Equal(Array.Empty<(int, int, int)>(), PythagoreanTriplet.TripletsWithSum(1001));
-    }
-
-    [Fact(Skip = "Remove to run test")]
-    public void Returns_all_matching_triplets()
-    {
-        Assert.Equal(new[]
+        [Fact]
+        public void No_matching_triplets_for_1001()
         {
+            Assert.Equal(Array.Empty<(int, int, int)>(), PythagoreanTriplet.TripletsWithSum(1001));
+        }
+
+        [Fact]
+        public void Returns_all_matching_triplets()
+        {
+            Assert.Equal(new[]
+            {
             (9, 40, 41),
             (15, 36, 39)
         }, PythagoreanTriplet.TripletsWithSum(90));
-    }
+        }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Several_matching_triplets()
-    {
-        Assert.Equal(new[]
+        [Fact]
+        public void Several_matching_triplets()
         {
+            Assert.Equal(new[]
+            {
             (40, 399, 401),
             (56, 390, 394),
             (105, 360, 375),
@@ -62,18 +65,19 @@ public class PythagoreanTripletTest
             (210, 280, 350),
             (240, 252, 348)
         }, PythagoreanTriplet.TripletsWithSum(840));
-    }
+        }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Triplets_for_large_number()
-    {
-        Assert.Equal(new[]
+        [Fact]
+        public void Triplets_for_large_number()
         {
+            Assert.Equal(new[]
+            {
             (1200, 14375, 14425),
             (1875, 14000, 14125),
             (5000, 12000, 13000),
             (6000, 11250, 12750),
             (7500, 10000, 12500)
         }, PythagoreanTriplet.TripletsWithSum(30000));
+        }
     }
 }
